e3a3d76607b48712fd5f12b1d342bb314a31773d
anggawanridho <55708748+anggawanridho@users.noreply.github.com>
2020-10-30 10:07:29 +0700

    Add files via upload

---
+++ src/cakep/PBO10119909Latihan30.java
@@ -0,0 +1,41 @@
+package cakep;
+
+import java.util.Scanner;
+
+/**
+ * @author
+ * NAMA     : Muhammad Anggawan Ridho Islami
+ * KELAS    : IF 10K
+ * NIM      : 10119909
+ */
+public class PBO10119909Latihan30 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String jawab;
+        String normal   = "\u001b[0m";
+        String merah    = "\u001b[31m";
+        String hijau    = "\u001b[32m";
+        String kuning   = "\u001b[33m";
+        String biru     = "\u001b[34m";
+        String ungu     = "\u001b[35m";
+        String birumuda = "\u001b[36m";
+
+        System.out.print(merah + "Kamu " + hijau + "ngerjain sendiri " + kuning + "latihan 17 sampe " + biru + "latihan 30 ini? \nJawab " + merah + "(Yoi/Enggak) : " + normal);
+        jawab = input.next();
+        jawab = jawab.toUpperCase();
+        System.out.println();
+        switch (jawab) {
+            case ("YOI"):
+                System.out.println(merah + "Cakep bener. " + ungu + "Good Job" + normal);
+                break;
+            case ("ENGGAK"):
+                System.out.println(merah + "Tetep cakep sih.\n" + birumuda + "Sing penting paham konsep nya yee.\n" + normal + "Keep the code works dude");
+                break;
+            default:
+                System.out.println(" ");
+        }
+    }
+}
